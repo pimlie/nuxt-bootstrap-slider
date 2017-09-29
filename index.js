921abc21e464d1bfb99e69fc5d73e50d3a408ebe
pimlie <pimlie@hotmail.com>
2017-09-29 22:58:23 +0200

    fix bailing hydration issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = function nuxtBootstrapSlider (options) {
   // Register plugin
-  this.addPlugin({ src: path.resolve(__dirname, 'plugin.js'), ssr: false })
+  this.addPlugin({ src: path.resolve(__dirname, 'plugin.js'), ssr: true })
 }
 
 module.exports.meta = require('./package.json')
